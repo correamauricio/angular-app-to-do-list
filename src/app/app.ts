21e344ad343510cfd6a54c5da38819d179d19d26
Mauricio Correa <correamauricio.design@gmail.com>
2025-11-22 22:24:10 -0300

    Commit inicial. Criado html geral

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, ListTodo, Plus } from 'lucide-angular';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
-  protected readonly title = signal('angular-app');
+  readonly ListTodo = ListTodo;
+  readonly Plus = Plus;
+  todoList: TodoItem[] = [];
+  newTask: string = '';
+
 }
