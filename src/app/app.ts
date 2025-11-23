5b2b38ef0355e6228bb3ed4bdbfbc3b432f26ca2
Mauricio Correa <correamauricio.design@gmail.com>
2025-11-23 01:16:13 -0300

    Adicionado funcionalidades: Add task, Deletar task, Concluir task

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { LucideAngularModule, ListTodo, Plus } from 'lucide-angular';
+import { LucideAngularModule, ListTodo, Plus, Trash2 } from 'lucide-angular';
 
 export interface TodoItem {
   id: number;
@@ -11,7 +12,7 @@ export interface TodoItem {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LucideAngularModule, FormsModule],
+  imports: [RouterOutlet, LucideAngularModule, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
@@ -19,7 +20,27 @@ export interface TodoItem {
 export class App {
   readonly ListTodo = ListTodo;
   readonly Plus = Plus;
+  readonly Trash2 = Trash2;
   todoList: TodoItem[] = [];
   newTask: string = '';
 
+  addTask():void {
+    if(this.newTask.trim() !== '') {
+      const newTodoItem:TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      };
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(task: TodoItem):void {
+    if (!this.todoList || !task) return;
+    const idx = this.todoList.findIndex(i => i.id === task.id);
+    if (idx >= 0) {
+      this.todoList.splice(idx, 1);
+    }
+  }
 }
